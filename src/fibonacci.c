2076956ab659d28e2fca0155eae9fc10b5111afa
Refat <refabatya@gmail.com>
2024-02-07 10:50:12 +0500

    done

--- src/fibonacci.c
+++ src/fibonacci.c
@@ -1,30 +1,70 @@
 #include <stdio.h>
+/*----------------------------------------------------------------------
+    Create a program src/quest3.c that calculates and outputs
+    display the nth Fibonacci number using a recursive algorithm.
+    The search for the nth Fibonacci number itself should be separated 
+    into a separate function. The number is entered via standard input.
+    When errors display “n/a”.
+
+
+    | Input data | Output data |
+    | ------ | ------ |
+        | 21 | 10946 |
+
+----------------------------------------------------------------------*/
 
 /*----------------------------------------------------------
-   Fibonacci example:
+    
+    Recursive function example:
 
-                int fibonacci(int a) {
+                int recursive(int a) {
                     if (a <= 1) {
                         return a;
                     }
                     printf("%d\n", a);
-                    return fibonacci(a - 1);
-                    printf("%d", fibonacci(21));
+                    return recursive(a - 1);
+                    printf("%d", recursive(21));
                 }
+
 ----------------------------------------------------------*/
 
-int main() {
-    int i, n;
-    int t1 = 0, t2 = 1;
-    int nextTerm = t1 + t2;
+/*-------------------------------------
+    
+    Option without recursive function
 
-    scanf("%d", &n);
-    for (i = 3; i <= n; ++i) {
-        t1 = t2;
-        t2 = nextTerm;
-        nextTerm = t1 + t2;
+        int main() {
+            int i, n;
+            int t1 = 0, t2 = 1;
+            int nextTerm = t1 + t2;
+
+            scanf("%d", &n);
+            for (i = 3; i <= n; ++i) {
+                t1 = t2;
+                t2 = nextTerm;
+                nextTerm = t1 + t2;
+            }
+            printf("%d", nextTerm);
+
+            return 0;
+        }
+
+-------------------------------------*/
+
+int fibonacci(int a) {
+    if (a <= 1) {
+        return a;
     }
-    printf("%d", nextTerm);
+    return fibonacci(a - 1) + fibonacci(a - 2);
+}
+
+int main() {
+    int n;
+
+    if (!scanf("%d", &n)) {
+        printf("n/a");
+    } else {
+        printf("%d", fibonacci(n));
+    }
 
     return 0;
 }
